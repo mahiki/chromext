6666d284f7fc066eeefcc1266f9c575f8629b682
mahiki <mahiki@users.noreply.github.com>
2015-12-13 10:33:50 -0800

    added kanye and kim

--- ex-er/blocker.js
+++ ex-er/blocker.js
@@ -4,8 +4,20 @@
  *
  ****************/
 
-$(":contains(Trump),:contains(TRUMP),:contains(trump),:contains(Kanye),:contains(Kardashian)")
+$(":contains(Trump),:contains(TRUMP),:contains(trump)")
 	.filter(function(){
 		return $(this).clone().children()
 			.remove().end().text().toLowerCase().indexOf("trump")!=-1;
+		}).remove();
+
+$(":contains(Kanye),:contains(KANYE),:contains(kanye)")
+	.filter(function(){
+		return $(this).clone().children()
+			.remove().end().text().toLowerCase().indexOf("kanye")!=-1;
+		}).remove();
+
+$(":contains(Kardashian),:contains(KARDASHIAN),:contains(kardashian)")
+	.filter(function(){
+		return $(this).clone().children()
+			.remove().end().text().toLowerCase().indexOf("kardashian")!=-1;
 		}).remove();
